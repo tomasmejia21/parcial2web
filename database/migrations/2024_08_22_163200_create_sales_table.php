73b6035e098651c85c2f7ab47196a31ea339308a
tomasmejia21 <tmejiag@unal.edu.co>
2024-08-21 12:05:54 -0500

    se crearon migraciones y algunos fix del proyecto

---
+++ database/migrations/2024_08_22_163200_create_sales_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->integer("value");
+            $table->string("movie");
+            $table->foreign("movie")->references("id")->on("movies");
+            $table->string("room");
+            $table->foreign("room")->references("id")->on("rooms");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
